752fcd7362e1b030913d80610db8db48322dbb5d
Luis Macena <luis.macena@outlook.com>
2022-09-23 18:41:33 -0300

    Add new branch dev

--- Scripts/urlisup.sh
+++ Scripts/urlisup.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-
 siteUp=$(ping -w 5 -ac 1 $1 2> /dev/null )
-if [[ $LANG == 'pt_BR.UTF-8' ]]; \
-then \
-lose="100% perda de pacote" \
-lost="Nome ou serviço desconhecido"; \
-else \
-lose="100% packet \
-loss" lost="unknown host"; \
-fi	
-if [[ ! $siteUp ]]; \
-then \
-  echo "$lost" ; \
-elif [[ $( echo $siteUp | grep "$lose") ]]; \
-then \
-  echo "$lose"; \
-else \
-  echo "Up";\
-fi
+unset $LANG $PYTHON
+
+PYTHON=$(which python)
+lose="100% packet loss" 
+lost="unknown host"; 
+
+if [[ ! $siteUp ]]; 
+then 
+    echo "$lost" ; 
+elif [[ $( echo $siteUp | grep "$lose") ]]; 
+then 
+    echo "$lose"; 
+else 
+    echo "Up";
+fi 
